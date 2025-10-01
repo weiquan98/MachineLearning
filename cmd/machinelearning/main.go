56e13de160669a023a4886834501b41afca3ef19
Quan <30767205+weiquan98@users.noreply.github.com>
2025-10-01 18:14:51 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/machinelearning/main.go
@@ -0,0 +1,23 @@
+// cmd/machinelearning/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "machinelearning/internal/machinelearning"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := machinelearning.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
